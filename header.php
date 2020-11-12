01caf1e373e53f1566ad2f7b07d93c7c10ee8c37
Alex Malave <alexyaniel34@gmail.com>
2020-11-12 17:42:28 -0500

    First

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <title>Groceries+</title>
+</head>
+<body>
+  
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include("header.php");
+?>
+
+<header class="hero">
+  <section class="container">
+    <h1 class="intro">
+      <span class="primary"><i class="fas fa-shopping-bag"></i></span>
+      <span style="color:#fff;">Groceries </span>
+      <span class="primary">+</span>
+    </h1>
+    <ul>
+      <li>
+        <button class="main-select button-primary">
+          Select Item
+        </button>
+      </li>
+      <li>
+        <button class="main-select button-primary">
+          Manage Foods
+        </button>
+      </li>
+      <li>
+        <button class="main-select button-primary">
+          Manage Stores
+        </button>
+      </li>
+    </ul>
+  </section>
+</header>
+
+<?php
+include("footer.php");
+?>
